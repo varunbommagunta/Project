a4e51a016cfd4a390f3e8d6437869256683e40ed
Va Run <65707153+varunbommagunta@users.noreply.github.com>
2024-04-15 17:19:01 +0530

    Update w_message.py

--- source_code/w_message.py
+++ source_code/w_message.py
@@ -2,5 +2,5 @@ import pywhatkit as kit
 from datetime import datetime
 	
 def send_alert(image_path):
-	phone_number = "+919182010324"
+	phone_number = "+999999999999"
 	kit.sendwhats_image(phone_number,img_path = image_path,caption = "Check this",wait_time = 7)
